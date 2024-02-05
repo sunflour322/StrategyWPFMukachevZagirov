5cb42d5e4dfeb7d26505a1fb1f7d4123f662f033
202111 <202111@1220-003-22>
2024-02-05 10:32:39 +0300

    Добавьте файлы проекта.

---
+++ StrategyWPFMukachev/Pages/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StrategyWPFMukachev.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+             string imagePath = "C:\\Users\\202111\\Desktop\\StrategyWPFMukachev\\doka2.jpg";
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(imagePath, UriKind.RelativeOrAbsolute);
+            bitmap.EndInit();
+            this.Background = new ImageBrush(bitmap);
+
+
+
+        }
+
+    }
+}
